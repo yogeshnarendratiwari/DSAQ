08c02961a4ca572c9c658d3e7537861bc7d1ea77
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-10-17 15:16:41 +0530

    String Q.2

---
+++ str2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool panlindrome(string s)
+{
+int start=0;
+int end = s.length()-1;
+bool result = true;
+while(start<=end)
+{
+    if(s.at(start)!=s.at(end))
+    result=false;
+    start++;
+    end--;
+    
+}
+return result;
+
+
+
+}
+int main()
+{
+    string name;
+    cin>>name;
+    // Panlindrome String
+    cout<<panlindrome(name)<<endl;
+}
