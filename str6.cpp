742119b5dd1f1bdf49274ed2ebb0a6ed66c1b85a
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-10-22 16:40:55 +0530

    queue & rotation of string

---
+++ str6.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1;
+    string s2;
+    cin >> s1 >> s2;
+    string s3 = s1 + s1;
+    if(s3.find(s2))
+    cout<<"Yes"<<endl;
+    else
+    cout<<"No"<<endl;
+}
